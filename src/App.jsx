f275115461f815f9bc68d7d1fac9a29a304c019e
Matheus Diógenes <mathdiogenes@gmail.com>
2024-06-30 13:53:20 -0300

    chore: react-router-dom package installation. refactor: pages and components directories. refactor: App.jsx content changed to pages/Employees.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,11 @@
-import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import Mobile from './pages/Mobile';
+import Employees from './pages/Employees';
 
-function App() {
-  const [state, setState] = useState({
-    isMobile: null,
-    isLoading: true,
-    windowDimensions: { width: null, height: null },
-  });
-
-  const getWindowDimensions = () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    return { width, height };
-  };
-
-  const handleWindowResize = () => {
-    setState({
-      ...state,
-      windowDimensions: getWindowDimensions(),
-    });
-  };
-
-  useEffect(() => {
-    const dimensions = getWindowDimensions();
-    setState((s) => ({
-      ...s,
-      windowDimensions: dimensions,
-      isLoading: false,
-    }));
-
-    window.addEventListener('resize', handleWindowResize);
-    return () => window.removeEventListener('resize', handleWindowResize);
-  }, []);
-
-  useEffect(() => {
-    const LARGE_DEVICE_WIDTH = 992;
-    const { width } = state.windowDimensions;
-
-    if (width !== null && width < LARGE_DEVICE_WIDTH) {
-      setState((s) => ({
-        ...s,
-        isLoading: false,
-        isMobile: true,
-      }));
-    } else {
-      setState((s) => ({
-        ...s,
-        isLoading: false,
-        isMobile: false,
-      }));
-    }
-  }, [state.isLoading, state.windowDimensions]);
-
-  if (state.isLoading) {
-    return (
-      <h1 style={ { color: 'red' } }>Loading</h1>
-    );
-  }
-
-  if (state.isMobile) {
-    return (
-      <Mobile />
-    );
-  }
+export default function App() {
   return (
-    <h1>Web</h1>
+    <Routes>
+      <Route path="/employees" element={ <Employees /> } />
+    </Routes>
   );
 }
-
-export default App;
---
+++ src/pages/Employees.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+
+import Mobile from '../components/Mobile';
+
+export default function Employees() {
+  const [state, setState] = useState({
+    isMobile: null,
+    isLoading: true,
+    windowDimensions: { width: null, height: null },
+  });
+
+  const getWindowDimensions = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    return { width, height };
+  };
+
+  const handleWindowResize = () => {
+    setState({
+      ...state,
+      windowDimensions: getWindowDimensions(),
+    });
+  };
+
+  useEffect(() => {
+    const dimensions = getWindowDimensions();
+    setState((s) => ({
+      ...s,
+      windowDimensions: dimensions,
+      isLoading: false,
+    }));
+
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  useEffect(() => {
+    const LARGE_DEVICE_WIDTH = 992;
+    const { width } = state.windowDimensions;
+
+    if (width !== null && width < LARGE_DEVICE_WIDTH) {
+      setState((s) => ({
+        ...s,
+        isLoading: false,
+        isMobile: true,
+      }));
+    } else {
+      setState((s) => ({
+        ...s,
+        isLoading: false,
+        isMobile: false,
+      }));
+    }
+  }, [state.isLoading, state.windowDimensions]);
+
+  if (state.isLoading) {
+    return (
+      <h1 style={ { color: 'red' } }>Loading</h1>
+    );
+  }
+
+  if (state.isMobile) {
+    return (
+      <Mobile />
+    );
+  }
+  return (
+    <h1>Web</h1>
+  );
+}
