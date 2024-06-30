94077fb2c6d2177f3274bca23cda2e72ddce2651
Matheus Diógenes <mathdiogenes@gmail.com>
2024-06-30 19:43:57 -0300

    style: Mobile.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import Employees from './pages/Employees';
 
+import './App.css';
+
 export default function App() {
   return (
     <Routes>
--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,34 +1,49 @@
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Mobile({ employeesData }) {
-  // useEffect(() => {
-  //   console.log(employeesData);
-  // }, []);
+import arrowDown from '../assets/arrow-down.svg';
+import logo from '../assets/header-logo.png';
 
+import style from '../styles/Mobile.module.css';
+
+const TABLE_CELL_STYLE = style['table-cell'];
+const TABLE_HEADING_STYLE = style['table-heading'];
+
+export default function Mobile({ employeesData }) {
   return (
-    <>
-      <h1>Funcionários</h1>
-      <input />
-      <table>
+    <div className={ style['outter-container'] }>
+      <header className={ style['header-container'] }>
+        <img src={ logo } alt="" className={ style['header-logo'] } />
+      </header>
+      <p className={ style.heading }>Funcionários</p>
+      <input type="text" />
+      <table className={ style['table-container'] }>
         <thead>
           <tr>
-            <th>Foto</th>
-            <th>Nome</th>
-            <th>•</th>
+            <th className={ TABLE_HEADING_STYLE }>FOTO</th>
+            <th className={ TABLE_HEADING_STYLE }>NOME</th>
+            <th className={ TABLE_HEADING_STYLE }>•</th>
           </tr>
         </thead>
         <tbody>
           {employeesData.map(({ id, name, image }) => (
             <tr key={ id }>
-              <td><img src={ image } alt="" /></td>
-              <td>{name}</td>
-              <td>BUTTON</td>
+              <td className={ TABLE_CELL_STYLE }>
+                <img src={ image } alt="" className={ style.photo } />
+              </td>
+              <td className={ TABLE_CELL_STYLE }>{name}</td>
+              <td className={ TABLE_CELL_STYLE }>
+                <input
+                  type="image"
+                  src={ arrowDown }
+                  alt=""
+                  className={ style['arrow-down'] }
+                />
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
 
